a327c76885d8df5bdea5fbf6166d0cfb57b1648f
dwjdwj1216 <dwjdwj2@126.com>
2015-07-30 17:28:39 +0800

    Chapter2

--- Checklists/ViewController.m
+++ Checklists/ViewController.m
@@ -23,5 +23,33 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 5;
+}
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ChecklistItem"];
+    UILabel *label = (UILabel *)[cell viewWithTag:1000];
+    if(indexPath.row == 0){
+        label.text = @"观看嫦娥飞天和玉兔升空视频";
+    }else if (indexPath.row == 1){
+        label.text = @"了解Sony a7和MBP的最新价格";
+    }else if (indexPath.row == 2){
+        label.text = @"复习苍老师的经典视频教程";
+    }else if (indexPath.row == 3){
+        label.text = @"去电影院看地心引力";
+    }else if (indexPath.row == 4){
+        label.text = @"看西甲巴萨新败的比赛回放";
+    }
+    return cell;
+}
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    if(cell.accessoryType == UITableViewCellAccessoryNone){
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    }else {
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
 
 @end
